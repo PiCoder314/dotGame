11e58f54dcd39fbb73f6bbeef37b8c831f422573
PiCoder314 <avguy1001@gmail.com>
2020-01-02 17:26:37 +0530

    Added new font "Nunito"

--- player.js
+++ player.js
@@ -1,27 +1,29 @@
 class Player {
-  constructor() {
+  constructor(mass) {
     this.pos = createVector(0,0)
     this.v = createVector(0,0)
     this.a = createVector(0,0)
-    this.m = 1
-    this.maxSpeed = 8
+    this.m = mass
+    this.maxSpeed = 9
+    this.damping = 0.05
   }
   update() {
     this.v.add(this.a)
     this.v.limit(this.maxSpeed)
     this.pos.add(this.v)
+    this.v.mult(this.damping)
     this.a.mult(0)
   }
-  intersects(threshold) {
-    return (this.pos.mag() >= threshold)
+  intersects(radius) {
+    return (this.pos.mag() >= radius)
   }
   applyForce(force) {
     const acc = force.div(this.m)
     this.a.add(acc)
   }
   show() {
-    fill(150, 0, 180)
     noStroke()
+    fill(57, 73, 171)
     circle(this.pos.x, this.pos.y, 6)
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,12 +9,13 @@ function setup() {
     windowWidth > windowHeight ? 
     windowHeight : windowWidth
   createCanvas(size, size);
-  dot = new Player()
-  frameRate(30)
+  dot = new Player(1)
+  frameRate(60)
+  textFont("Nunito")
 }
 function draw() {
   translate(width/2,height/2)
-  background(0)
+  background(31)
   stroke(255)
   noFill()
   circle(0, 0, width/1.2)
@@ -28,10 +29,9 @@ function draw() {
   }
   dot.update()
   dot.show()
-  if(dot.intersects(width/2.4)) {
+  if(dot.intersects(width/2.4))
     gameOver()
-  }
-  score += 1/30
+  score += 1/60
   fill(255)
   noStroke()
   textSize(32)
@@ -47,5 +47,6 @@ function gameOver() {
   fill(255)
   text("GAME OVER!!!", 0, 0)
   text(`Score: ${int(score)}` , 0, 32)
+  console.log("GAME OVER!!!")
   noLoop()
 }
